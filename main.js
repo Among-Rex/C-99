381db26a768725980e6cfa88203f955c24d7972b
Among-Rex <80576723+Among-Rex@users.noreply.github.com>
2021-08-19 18:32:21 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+    console.log(event);
+    var content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = content;
+    console.log(content);
+    //speak();
+    if (content == "take my selfie") {
+        console.log("Taking Selfie");
+        speak();
+    }
+}
+
+function speak() {
+    console.log("Inside Speak Function");
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'jpeg',
+    jpeg_quality:90
+});
